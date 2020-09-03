fd32058adadadeb08537b2335dfff5e2cab1256d
Peng Xiao <xiaoquwl@gmail.com>
2020-09-04 00:24:33 +0200

    change some base image

--- web/runserver.sh
+++ web/runserver.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-gunicorn --log-level info --log-file=/gunicorn.log --workers 4 --name app -b 0.0.0.0:8000 --reload app.app:app
+#gunicorn --log-level info --log-file=/gunicorn.log --workers 4 --name app -b 0.0.0.0:8000 --reload app.app:app
+gunicorn -c app/config.py app.app:app
